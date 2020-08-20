7a1e673a7f73ac18396a4c8819705393d488c93e
Tanner Tran <tran.tanner@yahoo.com>
2020-08-20 02:46:05 -0500

    Added endpoint for adding a new course for a teacher

---
+++ rest-service/src/main/java/com/teacherassistant/restservice/controller/CreateCourseController.java
@@ -0,0 +1,50 @@
+package com.teacherassistant.restservice.controller;
+
+import java.sql.SQLException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.teacherassistant.restservice.model.Course;
+import com.teacherassistant.restservice.service.CreateCourseService;
+
+@RestController
+public class CreateCourseController 
+{	
+	@PutMapping("/createCourse")
+	public ResponseEntity<String> createCourse(@RequestParam(value = "username") String username, @RequestBody Course course) throws SQLException
+	{
+		HttpStatus status;
+		
+		if (CreateCourseService.validateInput(course))
+		{
+			if (CreateCourseService.checkIfUsernameAvailable(username))
+			{
+				status = HttpStatus.NOT_FOUND;
+			}
+			else
+			{
+				if (CreateCourseService.checkIfCourseCodeAvailable(course.getCode(), username))
+				{
+					status = HttpStatus.CREATED;
+					CreateCourseService.createCourse(username, course.getCode(), course.getTitle());
+				}
+				else
+				{
+					status = HttpStatus.CONFLICT;
+					
+					return new ResponseEntity<>("Course code taken", status);
+				}
+			}
+		}
+		else
+		{
+			status = HttpStatus.BAD_REQUEST;
+		}
+		
+		return ResponseEntity.status(status).build();
+	}
+}
---
+++ rest-service/src/main/java/com/teacherassistant/restservice/service/CreateCourseService.java
@@ -0,0 +1,41 @@
+package com.teacherassistant.restservice.service;
+
+import java.sql.SQLException;
+
+import com.teacherassistant.restservice.dao.Database;
+import com.teacherassistant.restservice.model.Course;
+
+public class CreateCourseService 
+{
+	public static boolean validateInput(Course course)
+	{
+		boolean valid = true;
+		
+		if (course.getCode() == null || course.getCode().equals(""))
+		{
+			valid = false;
+		}
+		
+		if (course.getTitle() == null || course.getTitle().equals(""))
+		{
+			valid = false;
+		}
+		
+		return valid;
+	}
+	
+	public static boolean checkIfUsernameAvailable(String username) throws SQLException
+	{
+		return Database.checkIfUsernameAvailable(username);
+	}
+	
+	public static boolean checkIfCourseCodeAvailable(String code, String teacher) throws SQLException
+	{
+		return Database.checkIfCourseCodeAvailable(code, teacher);
+	}
+	
+	public static void createCourse(String teacher, String courseCode, String courseTitle) throws SQLException
+	{
+		Database.addCourse(courseTitle, courseCode, teacher);
+	}
+}
